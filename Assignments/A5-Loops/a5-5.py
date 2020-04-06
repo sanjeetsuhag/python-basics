79aa30c16d46c0c80676e95dac1954f73858cb25
Sanjeet Suhag <suhagsanjeet@gmail.com>
2020-04-05 23:57:46 -0400

    Fixes A5

--- Assignments/A5-Loops/a5-5.py
+++ Assignments/A5-Loops/a5-5.py
@@ -2,35 +2,35 @@ n = int(input('Enter n: '))
 
 lines = ""
 for i in range((2 * n) + 3):
-    lines = lines + "❤"
+    lines = lines + "🥔️"
 print(lines)
 
 for i in range(n):
-    output = "❤"
+    output = "🥔️️"
     for raccoon in range(n):
         output = output + "🦝"
-    output = output + "❤"
+    output = output + "🥔️️"
     for purdue_puppy in range(n):
         output = output + "🐶"
-    output = output + "❤"
+    output = output + "🥔️️"
     print(output)
 
 lines = ""
 for i in range((2 * n) + 3):
-    lines = lines + "❤"
+    lines = lines + "🥔️️"
 print(lines)
 
 for i in range(n):
-    output = "❤"
+    output = "🥔️️"
     for elephant in range(n):
         output = output + "🐘"
-    output = output + "❤"
+    output = output + "🥔️️"
     for leopard in range(n):
         output = output + "🐆"
-    output = output + "❤"
+    output = output + "🥔️️"
     print(output)
 
 lines = ""
 for i in range((2 * n) + 3):
-    lines = lines + "❤"
+    lines = lines + "🥔️️"
 print(lines)
